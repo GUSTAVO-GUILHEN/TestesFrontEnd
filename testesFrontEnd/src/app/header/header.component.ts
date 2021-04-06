f053c63845a69f8acdd06a2f0d9859c422008ba9
GUSTAVO-GUILHEN <gguilhen98@gmail.com>
2021-04-06 16:25:29 -0300

    Testes

---
+++ testesFrontEnd/src/app/header/header.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+
+    }
+
+    header_variable=false;
+    @HostListener("document:scroll")
+    scrollfunction(){
+        if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0 ){
+            this.header_variable=true;
+        }
+        else{
+            this.header_variable=false;
+        }
+    }
+
+
+}
